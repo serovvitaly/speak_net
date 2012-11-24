e9febfdda61f94b7560b68578b5d080245285e00
Vitaliy Serov <serovvitaly@gmail.com>
2012-11-24 19:54:48 +0400

    клон с pagodabox

--- modules/a1/classes/a1.php
+++
@@ -1,3 +0,0 @@
-<?php defined('SYSPATH') OR die('No direct access allowed.');
- 
-abstract class A1 extends A1_Core {}
--- modules/a1/classes/a1/orm.php
+++
@@ -1,3 +0,0 @@
-<?php defined('SYSPATH') or die('No direct access allowed.');
-
-class A1_ORM extends A1_Driver_ORM {}
